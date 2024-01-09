c56d58a8d07ccb762931576a832992e774296f0d
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-09 20:38:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,5 @@
 import os
 
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 18618422
+api_hash = "f165b1caec3cfa4df943fe1cbe82d22a"
+bot_token = "6331627404:AAHgjoOQqF4ClxNekEhFibzRVSslt_tsfsk"
